55a47791657035531b7286d9abdc27e1a040f7e6
Remy Manso <remzilla@Remys-MacBook-Pro.local>
2019-05-20 13:11:33 +0100

    using hash to store student data

--- directory.rb
+++ directory.rb
@@ -1,15 +1,16 @@
 # variables
-students_array = ["Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates"
+students_array  = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
 ]
 
 def print_header
@@ -20,7 +21,7 @@ end
 def print_student_list(names)
   # Here is the list of students
   names.each do |student|
-    puts student
+    puts "#{student[:name]} #{student[:cohort]} cohort"
   end
 end
 
