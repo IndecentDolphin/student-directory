025a8dd947da41b0cc47719886398eb23e5f2ee5
Remy Manso <remzilla@Remys-MacBook-Pro.local>
2019-05-20 12:29:40 +0100

    added directory file

---
+++ directory.rb
@@ -0,0 +1,18 @@
+# Here is the list of students
+puts "The students of Villains Academy"
+puts "-------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+# Printing the total amount of students 
+print "Overall, we have "
+print 11
+puts  " great students"
