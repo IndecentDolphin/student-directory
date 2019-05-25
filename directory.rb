cb8e59fbe68eee5f0d4b7d2fb6e842355ade4b2a
Remy Manso <remzilla@Remys-MacBook-Pro.local>
2019-05-25 13:27:40 +0100

    directory files containing the solutions to the various challenges. Not the neatest code but does what was asked

--- directory.rb
+++ directory.rb
@@ -1,43 +1,106 @@
 # variables
-students_array_backup  = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
-
 students_array = []
+@valid_months = [
+  "january",
+   "february",
+   "march",
+   "april",
+   "may",
+   "june",
+   "july",
+   "august",
+   "september",
+   "october",
+   "november",
+   "december"]
+
+def validate(cohort)
+  @valid_months.include?(cohort.downcase) ?  true : false
+end
+
+def get_cohort
+  puts "what cohort"
+  while true do
+    cohort = gets.chomp
+    if cohort.empty?
+      cohort = "november"
+    end
+    validation = validate(cohort)
+    puts "validation has returned #{validation}"
+    if validation == true
+      break
+    else
+      puts "invalid month, try again"
+    end
+  end
+  return cohort
+end
 
 
+# Challenge 5, added
 def add_student
   student = []
   puts "enter names of students to add"
   puts "when finished hit return twice"
   name = gets.chomp
+  cohort = get_cohort
+  puts "cohort is #{cohort}"
+  puts "what is their country of origin"
+  c_o_o = gets.chomp
 
+  if c_o_o.empty?
+    c_o_o = "N/A"
+  end
   while !name.empty? do
-    student << {name: name, cohort: :november}
+    # cohort = cohort.to_sym
+    student << {name: name, cohort: cohort.to_sym, country_of_origin: c_o_o}
+    puts "next name:"
     name = gets.chomp
+    cohort = get_cohort
+    puts "country of origin?"
+    c_o_o = gets.chomp
   end
   student
 end
 
+
+# Challenge 8, group the students
+def student_grouping(array)
+  grouped_by_cohort = {}
+  array.each do |student|
+    cohort = student[:cohort]
+    if grouped_by_cohort[cohort] == nil
+      grouped_by_cohort[cohort] = []
+    end
+    grouped_by_cohort[cohort].push(student[:name])
+  end
+  grouped_by_cohort.each do |key, value|
+    s_key = key.to_s
+    s_key.capitalize!
+    puts "----------------------"
+    puts "#{s_key} cohort".center(20)
+    puts "----------------------"
+    value.each do |name|
+      puts name.center(20)
+    end
+  end
+end
+
 def print_header
   puts "The students of Villains Academy"
-  puts "-------------"
+  puts "------------------------------------------------"
 end
+
 
 def print_student_list(names)
   # Here is the list of students
-  names.each do |student|
-    puts "#{student[:name]} #{student[:cohort]} cohort"
+  # Here is challenge 12
+  if !names.empty?
+    names.each do |student|
+      puts "#{student[:name]} #{student[:cohort]} cohort"
+    end
+  else
+    puts "There are no students to display :o"
   end
 end
 
@@ -48,5 +111,6 @@ end
 
 students_array = add_student
 print_header
-print_student_list(students_array)
+# print_student_list(students_array)
+student_grouping(students_array)
 print_footer(students_array)
--- directory_2.rb
+++ directory_2.rb
@@ -1,27 +1,31 @@
 # variables
+# test data
 students_array  = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
+  {name: "Dr. Hannibal Lecter", cohort: :november, height: "6ft"},
+  {name: "Darth Vader", cohort: :november, height: "7ft"},
+  {name: "Nurse Ratched", cohort: :november, height: "6ft"},
+  {name: "Michael Corleone", cohort: :janurary, height: "6ft"},
+  {name: "Alex DeLarge", cohort: :november, height: "6ft"},
+  {name: "The Wicked Witch of the West", cohort: :janurary, height: "3ft"},
+  {name: "Terminator", cohort: :november, height: "7ft"},
+  {name: "Freddy Krueger", cohort: :november, height: "6ft"},
+  {name: "The Joker", cohort: :november, height: "6ft"},
+  {name: "Joffrey Baratheon", cohort: :janurary, height: "4ft"},
+  {name: "Norman Bates", cohort: :november, height: "6ft"}
 ]
 
+def add_student
 
+end
 
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
 end
 
-def print_student_list(names)
-  # Here is the list of students, printed with index, and configured to select student based on Initial name letter
+def print_student_list_name_filter(names)
+  # Here is the list of students, printed with index, and configured to
+  # select student based on Initial name letter
   names.each_with_index do |student, index|
     if student[:name].start_with?("T")
       puts "#{index + 1}. #{student[:name]} #{student[:cohort]} cohort"
@@ -29,11 +33,60 @@ def print_student_list(names)
   end
 end
 
+# Challenge 10, another way to modify the string
+def clean_string_savage
+  name = gets
+  name.tr!("\n","")
+  puts name
+end
+
+# Challenge 3, dependant on name length
+def print_students_name_length_filter(names)
+  names.each do |student|
+    if student[:name].length <= 12
+      puts  " #{student[:name]} #{student[:cohort]} cohort"
+    end
+  end
+end
+
+# Challenge 4, rewrite the .each method to more barbaric ways of controlling flow
+def print_student_list_savage(array)
+  i = 0
+  while i < array.length do
+    puts array[i][:name]
+    i += 1
+  end
+end
+
+# Challenge 6, pretty formatting for output
+def print_student_list_pretty(names)
+  names.each_with_index do |student, index|
+    print "#{(index + 1).to_s} ".center(4)
+    print "#{student[:name]}".center(30)
+    print "Height: #{student[:height]}".center(20)
+    puts "#{student[:cohort]} cohort".center(20)
+
+    # puts "#{index + 1}. #{student[:name]} height:#{student[:height]} #{student[:cohort]} cohort".center(20, "x")
+  end
+end
+
+
+
+
 def print_footer(names)
   # Printing the total amount of students
-  puts "Overall, we have #{names.count} great students"
+  puts ""
+  # For Challenge 9
+  if names.count > 1
+    puts "Overall, we have #{names.count} great students"
+  else
+    puts "Overall, we have #{names.count} great student"
+  end
 end
 
 print_header
-print_student_list(students_array)
+# print_student_list(students_array)
+print_student_list_pretty(students_array)
+# print_students_name_length(students_array)
+# print_student_list_savage(students_array)
 print_footer(students_array)
